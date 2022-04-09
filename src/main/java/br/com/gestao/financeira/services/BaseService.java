091929f0fa541a3a4c063b7670d04cb0564cfd1c
Higorbraga01 <higorbraga00@gmail.com>
2022-04-09 17:48:23 -0300

    Implementa BaseService

--- src/main/java/br/com/gestao/financeira/services/BaseService.java
+++ src/main/java/br/com/gestao/financeira/services/BaseService.java
@@ -1,4 +1,16 @@
 package br.com.gestao.financeira.services;
 
-public class BaseService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.querydsl.core.types.Predicate;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseService<T> {
+    T create(T dto);
+    T update(Long id, T dto);
+    void delete(Long id);
+    Page<T> findAll(Predicate predicate, Pageable pageable);
+    List<T> findAll();
+    Optional<T> findById(Long id);
 }
