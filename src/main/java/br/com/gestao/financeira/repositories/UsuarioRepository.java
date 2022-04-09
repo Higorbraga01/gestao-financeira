cfa7ea523bb29154dc676fdef414598d693dcad7
Higorbraga01 <higorbraga00@gmail.com>
2022-04-09 19:27:01 -0300

    Adiciona interface QuerydslBinderCustomizer ao repository

--- src/main/java/br/com/gestao/financeira/repositories/UsuarioRepository.java
+++ src/main/java/br/com/gestao/financeira/repositories/UsuarioRepository.java
@@ -1,11 +1,26 @@
 package br.com.gestao.financeira.repositories;
 
+import br.com.gestao.financeira.models.QUsuario;
 import br.com.gestao.financeira.models.Usuario;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>, QuerydslPredicateExecutor<Usuario>, JpaSpecificationExecutor<Usuario> {
+public interface UsuarioRepository extends
+        JpaRepository<Usuario, Long>,
+        QuerydslPredicateExecutor<Usuario>,
+        JpaSpecificationExecutor<Usuario>,
+        QuerydslBinderCustomizer<QUsuario> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QUsuario usuario) {
+        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 }
