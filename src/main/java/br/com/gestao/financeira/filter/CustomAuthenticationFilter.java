7c8689a3243c15e702744d90c7e40d5d98bf7da9
Higorbraga01 <higorbraga00@gmail.com>
2023-03-13 12:42:50 -0300

    remove log de username e password

--- src/main/java/br/com/gestao/financeira/filter/CustomAuthenticationFilter.java
+++ src/main/java/br/com/gestao/financeira/filter/CustomAuthenticationFilter.java
@@ -39,8 +39,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String userName = request.getParameter("username");
         String password = request.getParameter("password");
-        log.info("Username is: {}", userName);
-        log.info("Password is: {}", password);
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userName, password);
         return authenticationManager.authenticate(authenticationToken);
     }
